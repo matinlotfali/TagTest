88686f2f6cd100bf47e1f370b172bb16c9800bec
Matin Lotfaliei <matinlotfali@gmail.com>
2022-12-20 14:00:53 -0800

    Add Tags Widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "tags/tags_line_edit.hpp"
+#include "tags/tags_edit.hpp"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    auto tagsLineEdit = new yenxo_widgets::TagsLineEdit(true, ui->centralwidget);
+    tagsLineEdit->setObjectName(QString::fromUtf8("myTagsLineEdit"));
+    tagsLineEdit->setGeometry(QRect(190, 70, 251, 80));
+    tagsLineEdit->completion({"test1", "test2", "test3"});
+    tagsLineEdit->tags({"test0", "test1"});
+
+    auto tagsEdit = new yenxo_widgets::TagsEdit(true, ui->centralwidget);
+    tagsEdit->setObjectName(QString::fromUtf8("myTagsEdit"));
+    tagsEdit->setGeometry(QRect(190, 170, 251, 80));
+    tagsEdit->completion({"test1", "test2", "test3"});
+    tagsEdit->tags({"test0", "test1"});
 }
 
 MainWindow::~MainWindow()
